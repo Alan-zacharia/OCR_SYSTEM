22b3c50d9a540fe396fff152cec5e7d1d33cf0ff
Alan Zacharia <alanzacaharia@gmail.com>
2024-10-24 19:55:54 +0530

    Updated API Integration

--- server/src/config/envConfig.ts
+++ server/src/config/envConfig.ts
@@ -3,6 +3,7 @@ dotenv.config();
 
 const envConfig =  {
      PORT : process.env.PORT || 3000 as number,
+     CLIENT_URL : process.env.CLIENT_URL 
 };
 
 export default envConfig;
--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: envConfig.CLIENT_URL,
   })
 );
 app.use(morgan("dev"));
